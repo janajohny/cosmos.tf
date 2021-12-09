8309358dd24d702ce83e2313711ce5bce1adf108
devbatch2b <devbatch2b@hotmail.com>
2021-12-09 19:11:46 +0530

    updated

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "subscription_id" {
+  default = "95f5a666-257f-4180-beaa-2b569ca0104b" 
+}
+#variable "tenant_id" {
+#  default = "e2435251-08b0-4e7e-b5a3-c78724fadfe4"
+#}
+
+variable "resource_group_name" {
+  default = "multi-cloud-tooling"
+}
+
+variable "resource_group_location" {
+  default = "eastus2"
+}
+
+variable "cosmos_db_account_name" {
+  default = "mrnewcosmosmongodb"
+}
+
+variable "failover_location" {
+  default = "eastus"
+}
+
